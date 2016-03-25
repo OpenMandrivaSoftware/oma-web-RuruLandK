ea5c1d3f7fc2485e870cf6325eb4f65e290223a4
raphael <rj@hodo.fr>
2016-03-25 16:07:01 +0100

    clean language

--- lang/donate_en.php
+++ lang/donate_en.php
@@ -1,15 +1,19 @@
-  <?php
-  $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'old_like' => 'If you like OpenMandriva and appreciate our work, you can give us your love and care by donating your time or money. <br />
-              Donate your time by joining us in our effort to build the best Linux distribution or by becoming part of our wonderful, friendly
-              <a href=forum.openmandriva.org>community</a>. Or you can simply choose to donate money, that will help us with our costs.
-              In case you plan to donate more generously, check out our wiki page on <a href="//wiki.openmandriva.org/en/Donation_and_Sponsorship">Donation</a>.',
+<?php
+// This is a SPIP language file
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // L
     'like'  =>    'We, at OpenMandriva, enjoy working to bring to you a first-class and well-maintained Linux distribution.
-                    We work passionately as part of and for a thriving open source community.<br />
-                    As we are not a commercial organization, we do not do paid work and contribute to the project during our free time.<br />
-                    You can support us financially by donating: your contribution could be as little as buying us a glass of beer to as much as being enough to make you qualify as a sponsor.<br />
-                    So if you like OpenMandriva and appreciate our work, go ahead and show your love by donating through the button below. If you\'re interested in becoming a sponsor, please contact us at contact@openmandriva.org
-                    Your name will also be displayed on donation websites, unless you wish not.'
+                      We work passionately as part of and for a thriving open source community.<br />
+                      As we are not a commercial organization, we do not do paid work and contribute to the project during our free time.<br />
+                      You can support us financially by donating: your contribution could be as little as buying us a glass of beer to as much
+                       as being enough to make you qualify as a sponsor.<br />
+                      So if you like OpenMandriva and appreciate our work, go ahead and show your love by donating through the button below.
+                      If you\'re interested in becoming a sponsor, please contact us at contact@openmandriva.org
+                      Your name will also be displayed on donation websites, unless you wish not.',
+
   );
   ?>
