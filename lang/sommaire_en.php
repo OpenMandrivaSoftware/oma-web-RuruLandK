07a6fc0848562f42a14fdf430d50012453ca724e
Raphaël <rj@hodo.fr>
2016-03-28 00:53:53 +0200

    lot of new files added

---
+++ lang/sommaire_en.php
@@ -0,0 +1,32 @@
+<?php
+// This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // Big picture [bp]
+    'bp00'  =>    'Here comes',
+    'bp01'  =>    'Lx 3',
+    'bp10'  =>    'We proudly present our new OpenMandriva Lx release, long awaited, but faster than ever!',
+
+  // Promotion table [pt]
+    'pt00'  =>    '——DEVS—— WANDBOARD',
+    'pt01'  =>    'Image for wandboard.org OpenMandriva added as officialy supported distro to wandboard download page.',
+    'pt10'  =>    '——USERS——<br /> TOUR',
+    'pt11'  =>    'Explore OpenMandriva Features, discover its secrets.',
+    'pt20'  =>    '——PLAYERS—— GAMES',
+    'pt21'  =>    'Love games? Even Windows games work well on OMA! Check it out here.',
+    'pt30'  =>    '——DUMMIES—— GUIDE',
+    'pt31'  =>    'New here? Discover it simple, with our Guide for newbies!.',
+
+  // Association presentation [ap]
+    'ap00'  =>    'The OpenMandriva Association',
+    'ap01'  =>    'The OpenMandriva Association\'s goal is to develop a pragmatic distribution that provides the best user experience for everyone,
+                  from newbies to developers. We will achieve our objectives for the best balance between the most modern features and stability.',
+    'ap02'  =>    'Our roots are in Mandrake and its traditions, we are a worldwide community of people who are passionate about free software
+                  working together and take our technical and strategic decisions in a collaborative manner. We do not just build a Linux distro,
+                  we exchange knowledge and make new friends. Welcome to OpenMandriva Community! Click here to know who are us and what we are doing.',
+
+  );
+  ?>
