d32765967544c92d50b4465718fe1a31c8168082
raphael <rj@hodo.fr>
2016-04-13 17:33:08 +0200

    dunno

---
+++ lang/donate_fr.php
@@ -0,0 +1,19 @@
+<?php
+// This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  // L
+    'like'  =>    'We, at OpenMandriva, enjoy working to bring to you a first-class and well-maintained Linux distribution.
+                      We work passionately as part of and for a thriving open source community.<br />
+                      As we are not a commercial organization, we do not do paid work and contribute to the project during our free time.<br />
+                      You can support us financially by donating: your contribution could be as little as buying us a glass of beer to as much
+                       as being enough to make you qualify as a sponsor.<br />
+                      So if you like OpenMandriva and appreciate our work, go ahead and show your love by donating through the button below.
+                      If you\'re interested in becoming a sponsor, please contact us at contact@openmandriva.org
+                      Your name will also be displayed on donation websites, unless you wish not.',
+
+  );
+  ?>
